869de76c47665597beadb58d1549e42bce7302b0
Sandy <sandy.jernigan@gmail.com>
2020-03-18 04:18:28 -0400

    Tests passed

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -14,7 +14,7 @@ class LRUCache:
         # current number of nodes - start with 0
         self.size = 0
         # DLL that holds the key-value entries
-        self.dll = DoublyLinkedList()
+        self.order = DoublyLinkedList()
         # storage dict
         self.storage = {}
         # Most Recently Used Node
@@ -22,40 +22,15 @@ class LRUCache:
         # Oldest Used Node
         self.tail = None
 
-    """
-    Retrieves the value associated with the given key. Also
-    needs to move the key-value pair to the end of the order
-    such that the pair is considered most-recently used.
-    Returns the value associated with the key or None if the
-    key-value pair doesn't exist in the cache.
-    """
     def get(self, key):
         # if key exists, return the Node
         if key in self.storage:
-            # Retrieves the value associated with the given key
+            # Get the Node
             node = self.storage[key]
-            results = self.storage[key].value
-
-            # if node is already in most recent, just return value
-            if node is self.head: return results
-
-            # Remove Node by resetting Connections around Node
-            if node.next: node.next.prev = node.prev
-            if node.prev: node.prev.next = node.next
-
-            # If Node is the tail, set a new tail
-            if node is self.tail:
-                self.tail = self.tail.prev
-
-            # Update head node
-            self.head.prev = node
-            node.next = self.head
-            node.prev = None
-            self.head = node
-
-            # Return the value associated with the key
-            return results
-
+            # Move to Front
+            self.order.move_to_front(node)
+            # Returns the value associated with the key
+            return node.value[1]
         else:
             # Returns None if the key-value pair doesn't exist in the cache
             return None
@@ -73,30 +48,26 @@ class LRUCache:
     def set(self, key, value):
         # If key already exists in cache, overwrite the value
         if key in self.storage:
-            self.storage[key].value = value
-        else:
-            # if key doesn't exist
-            # Add the given key-value pair to the cache to the head
-            self.storage.add_to_head(key, value)
-            self.storage[key] = self.head
-            print(self.get(key))
-            print(self.storage)
-        
-        # If Not at max capacity, update size
-        if self.size < self.limit:
-            self.size += 1
+            # Get the Node
+            node = self.storage[key]
+            # Overwrite value
+            node.value = (key, value)
+            # Move to Front
+            self.order.move_to_front(node)
+            # Exit
+            return
         
         # If at max capacity, remove oldest entry
-        else:
-            # Get key of current tail
-            tailKey = self.tail.key
-
-            # Adjust the tail position
-            self.tail = self.tail.prev
-            self.tail.next = None
-
+        if self.size == self.limit:
             # delete old tail
-            del self.storage[tailKey]
+            del self.storage[self.order.tail.value[0]]
+            self.order.remove_from_tail()
+            self.size -= 1
+
+        # Add the given key-value pair to the cache to the head
+        self.order.add_to_head((key, value))
+        self.storage[key] = self.order.head
+        self.size += 1
         
         # Get Results and Set to most-recently used entry (head)
         return self.get(key)
